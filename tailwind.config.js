0247ac1af618f82c2e3201d02121357849246467
Lucía Lázaro <lucia@ip-172-28-0-142.eu-west-1.compute.internal>
2021-12-31 18:20:55 +0100

    Push de prueba

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,9 +20,31 @@ module.exports = {
       'xl': '1280px',
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+      beige: '#fff8dc',
+      negro: '#101010',
+      salmon: '#f98c8c',
+      gris: '#212529',
+      amarillo: '#f6e05e',
+      gris2: '#272822',
+      azul: '#4481D8',
+      blanco: '#F8F9FB',
+      rojo: '#F06451',
+      rojo2: '#F62020',
+      gris3: '#F3ECEB',
+      gris4: '#999898',
+      verde: '#198754',
+      verde2: '#12643E',
+      verde3: '#0E5032',
+      gris5: '#ADADAD'
+    },
 
+
+    fontFamily: {
+      nunito: ['Nunito', 'sans-serif'],
+      leck: ['Leckerli One', 'cursive'],
+      undog: ['Underdog', 'sans-serif']
     },
     extend: {},
   },
